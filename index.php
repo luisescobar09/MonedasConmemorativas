75cb300d99c6183b2517facd83b353f0c70c5f84
José Luis Escobar Pérez <1719110043@utectulancingo.edu.mx>
2023-04-20 06:46:57 -0600

    Update files

--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <?php 
     require_once("index.html"); 
     require_once("/");
-    require_once("css/");
-    require_once("js/");
-    require_once("images/");
-    require_once("pages/");
 
-    
+    require_once("css/");
+    require_once("css/main.css");
+    require_once("css/materialize.min.css");
+    require_once("css/styles.css");
+
+    require_once("js/");
+    require_once("js/app.js");
+    require_once("js/materialize.min.js");
+    require_once("js/monedas.js");
+    require_once("js/ui.js");
+
+    require_once("images/");
+    require_once("images/icons/bicentenario_pf.png");
+    require_once("images/icons/icon-72x72.png");
+    require_once("images/icons/icon-92x92.png");
+    require_once("images/icons/icon-96x96.png");
+    require_once("images/icons/icon-128x28.png");
+    require_once("images/icons/icon-144x144.png");
+    require_once("images/icons/icon-152x152.png");
+    require_once("images/icons/icon-192x192.png");
+    require_once("images/icons/icon-384x384.png");
+    require_once("images/icons/icon-512x512.png");
+
+
+    require_once("pages/");
+    require_once("pages/about.html");
+
+
     require_once("admin/");
     require_once("admin/index.php");
 ?>
